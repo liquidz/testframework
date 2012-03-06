2a2e27f1078dc4b088f5b1d3d8d53303828bc702
Masashi Iizuka <liquidz.uo@gmail.com>
2012-03-07 01:59:11 +0900

    delete unnecessary code

--- misaki-core/test/misaki/test/core.clj
+++ misaki-core/test/misaki/test/core.clj
@@ -1,13 +1,6 @@
 (ns misaki.test.core
-  (:use misaki)
 ;  (:use [misaki.server])
   (:use [clojure.test]))
 
 (deftest replace-me
-
-  (println (json 1))
-  (println (json "hello"))
-  (println (json [1 2 "3"]))
-  (println (json {:a 1 :b "2"}))
-
   (is true))
