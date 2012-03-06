677b2f8bd8ea85b0a268649d209e24faf31e130c
Masashi Iizuka <liquidz.uo@gmail.com>
2012-03-07 00:21:20 +0900

    fix validation. add validation test code.

--- misaki-core/src/misaki/validation.clj
+++ misaki-core/src/misaki/validation.clj
@@ -1,41 +1,54 @@
 (in-ns 'misaki)
 
 (declare
-  ^{:dynamic true}
+  ^{:dynamic true
+    :doc "atom for validation errors"}
   *validate-errors*)
 
-(def ^{:dynamic true}
+(def ^{:dynamic true
+       :doc "atom for last validation errors"}
   *last-validate-errors* (atom nil))
 
-(defmacro validate! [pred msg]
+(defmacro validate!
+  "validate predication"
+  [pred msg]
   `(when (not ~pred)
      (swap! *validate-errors* conj ~msg)))
 
-(defn validation-keep!
-  "keep validation result to next access"
+(defn keep-validation!
+  "keep validation result to next binding"
   []
   (reset! *last-validate-errors* @*validate-errors*))
 
 (defn has-error?
-  ([]     (has-error? *validate-errors*))
-  ([atom] (not (empty? @atom))))
+  "return whether errors are raised or not"
+  []
+  (-> @*validate-errors* empty? not))
 
-(defn get-errors []
+(defn get-errors
+  "return validation errors"
+  []
   @*validate-errors*)
 
-(defn make-validation-atom []
+(defn make-validation-atom
+  "return validation atom considering keeped errors"
+  []
   (if-let [last-error @*last-validate-errors*]
     (do (reset! *last-validate-errors* nil)
         (atom last-error))
     (atom [])))
 
+(defmacro with-validation
+  "validation error binding macro"
+  [& body]
+  `(binding [*validate-errors* (make-validation-atom)]
+     ~@body))
+
 (defn wrap-validation
-  "middleware for ring"
+  "ring middleware for validation"
   [handler]
   (fn [request]
-    (binding [*validate-errors* (make-validation-atom)]
-      (handler request))))
-
+    (with-validation (handler request))))
 
 (defn required? [x]
   (and (not (nil? x))
--- misaki-core/test/misaki/test/validation.clj
+++ misaki-core/test/misaki/test/validation.clj
@@ -1,61 +1,40 @@
 (ns misaki.test.validation
-  (:use [misaki.validation])
+  (:use misaki)
   (:use [clojure.test]))
 
-(deftest required-test
-  (let [x nil, y "", z 10, f (constantly nil)]
-    ; invalid pattern
-    (is (= "a is required"     ((required :a) {})))
-    (is (= "test"              ((required :a "test") {})))
-    (is (= "\"a\" is required" ((required "a") {})))
-    (is (= "nil is required"   ((required nil) {})))
-    (is (= "nil is required"   ((required (f)) {})))
+(deftest validation-test
+  (with-validation
+    (validate! (nil? nil) "dummy")
+    (is (not (has-error?)))
 
-    ; valid pattern
-    (is (nil? ((required :a) {:a 1})))
-    (is (nil? ((required 'a) {'a 1})))
-    (is (nil? ((required "a") {"a" 1})))
+    (validate! (nil? 1) "test")
+    (is (has-error?))
+    (is (= ["test"] (get-errors)))
 
-    ))
+    (validate! (= 1 2) "test2")
+    (is (= ["test" "test2"] (get-errors)))))
 
+(defn myvalidate [x]
+  (validate! (= x 10) "x is not 10"))
 
-(deftest equals-test
-  (let [f (constantly nil)]
-    ; invalid pattern
-    (is (= "nil is not nil, but 0"   ((equals nil nil) {nil 0})))
-    (is (= "a is not false, but 0"   ((equals :a false) {:a 0})))
-    (is (= "a is not \"nil\", but 0" ((equals :a "nil") {:a 0})))
-    (is (= "test"                    ((equals :a "nil" "test") {:a 0})))
-    (is (= "nil is not 1, but 0"     ((equals (f) 1) {nil 0})))
+(deftest outer-validation-test
+  (with-validation
+    (myvalidate 10)
+    (is (not (has-error?)))
 
-    ; valid pattern
-    (is (nil? ((equals :a true) {:a true})))
-    (is (nil? ((equals :a nil) {:b "dummy"}))) ; must be checked by "required"
-    (is (nil? ((equals :a ((constantly 10))) {:a 10})))
+    (myvalidate 5)
+    (is (= ["x is not 10"] (get-errors)))))
 
-    ))
+(deftest keep-validation-test
+  (with-validation
+    (is (not (has-error?)))
+    (validate! (= 1 2) "test")
+    (keep-validation!))
 
+  (with-validation
+    (is (has-error?))
+    (is (= ["test"] (get-errors)))
 
-(deftest validate-test
-  ; invalid pattern
-
-  (is (= (validate {:a true} (required :b "test"))
-         (list "test")))
-
-  (is (= (validate {:a true} (required :a) (required :b "test"))
-         (list "test")))
-
-  (is (= (validate {:a true} (required :b "test") (required :a))
-         (list "test")))
-
-  (is (= (validate {:a true} (required :b "test") (equals :a false "test2"))
-         (list "test" "test2")))
-
-  ; valid pattern
-  (is (nil? (validate {:a true} (required :a))))
-  (is (nil? (validate {:a true} (required :a) (equals :a true))))
-
-  )
-
-
+    (validate! false "test2")
+    (is (= ["test" "test2"] (get-errors)))))
 
