1e5ccd20a4ad430560235861b1b690b4dd4c9338
Masashi Iizuka <liquidz.uo@gmail.com>
2012-03-14 00:11:22 +0900

    add test supporting functions

--- misaki-core/project.clj
+++ misaki-core/project.clj
@@ -1,4 +1,4 @@
-(defproject misaki "0.0.9.2-alpha"
+(defproject misaki "0.0.9.3-alpha"
   :description "misaki - library package to build web application easily"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.1"]
@@ -14,18 +14,18 @@
                  [korma "0.2.1"]
                  [mysql/mysql-connector-java "5.1.13"]]
 
-;  :dev-dependencies [[codox "0.4.1"]
-;                     [ring-mock "0.1.1"]]
+  :dev-dependencies [[codox "0.4.1"]
+                     [ring-mock "0.1.1"]]
 
   ; for example
-  :dev-dependencies [[lein-ring "0.5.4"]
-                     [ring-mock "0.1.1"]
-                     ; doc
-                     [codox "0.4.1"]
-                     ]
-  :ring {:handler exampleserver/handler}
-  :main exampleserver
-  :codox {:exclude [example.app server]}
+;  :dev-dependencies [[lein-ring "0.5.4"]
+;                     [ring-mock "0.1.1"]
+;                     ; doc
+;                     [codox "0.4.1"]
+;                     ]
+;  :ring {:handler exampleserver/handler}
+;  :main exampleserver
+;  :codox {:exclude [example.app server]}
   ; /for example
 
   :aot [misaki.core])
--- misaki-core/src/misaki/util/test.clj
+++ misaki-core/src/misaki/util/test.clj
@@ -1,18 +1,33 @@
 (ns misaki.util.test
-  (:use ring.mock.request))
+  (:use
+    [misaki.core :only [misaki-app-symbol]]
+    ring.mock.request)
+  (:require [clojure.string :as str]))
 
-(defn send-request
+
+(defmacro send-request
   "send request to misaki's default route(misaki-app)"
   [& args]
-  (misaki-app (apply request args)))
+  (let [args (if (keyword? (first args))
+               args (cons :get args))]
+    `(~misaki-app-symbol (request ~@args))))
 
-(defn status-ok? [req]
-  (= 200 (:status req)))
+(defn status-ok? [res]
+  (= 200 (:status res)))
 
-(defn header= [key value]
+(defn content-type= [res value]
+  (= value (first (str/split (get (:headers res) "Content-Type") #";"))))
+
+(defn header= [res key value]
   (let [key (if (keyword? key) (name key) key)]
-    (= value (get (:header req) key))))
+    (= value (get (:headers res) key))))
 
-(defn body-contains? [s]
+(defn header-contains? [res key value]
+  (let [key (if (keyword? key) (name key) key)]
+    (println "a:" (get (:headers res) key))
+    (println "b:" (:headers res))
+    (not= -1 (.indexOf (get (:headers res) key "") value))))
+
+(defn body-contains? [req s]
   (not= -1 (.indexOf (:body req) s)))
 
